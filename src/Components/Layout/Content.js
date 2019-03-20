c58d3c93820e6d8fbd46dd21944c187ba6cccaa7
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2019-03-20 10:47:23 +0530

    Content area for the tcupexplorer directory application

---
+++ src/Components/Layout/Content.js
@@ -0,0 +1,140 @@
+import React from "react";
+
+const Content = props => (
+  <div>
+    <div className="content-wrap">
+      <div className="main">
+        <div className="container-fluid">
+          <div className="row" />
+
+          <div className="content">
+            <div className="row">
+              <div className="col -md -12" />
+            </div>
+
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="content all-icons">
+                    <div className="row ">
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder1" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder2" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder3" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder4" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="folder3" />
+                        </div>
+                      </div>
+                      <div className="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                        <div className="font-icon-detail">
+                          <i
+                            className="fa fa-folder  fa-3x"
+                            aria-hidden="true"
+                          />
+                          <input type="text" value="delete" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Content;
